1786400097bb17f333db19bdf08bf4d474183fa1
Nicolette47 <nvanzeijl@gmail.com>
2025-03-21 22:02:22 +0100

    ingredienten ingevoerd en werken

---
+++ src/ApplePieRecipe.java
@@ -0,0 +1,16 @@
+public class ApplePieRecipe {
+
+
+    Ingredient roomboter  = new Ingredient (200, "gram", "ongezouten roomboter");
+    Ingredient suiker  = new Ingredient (200, "gram", "witte basterd suiker");
+    Ingredient bakmeel  = new Ingredient (400, "gram", "zelfrijzend bakmeel");
+    Ingredient ei  = new Ingredient (1, "stuk(s)", "ei");
+    Ingredient vanillesuiker  = new Ingredient (8, "gram", "vanillesuiker");
+    Ingredient zout  = new Ingredient (1, "snuf", "zout");
+    Ingredient appels  = new Ingredient (1.5, "kilo", "zoetzure appels");
+    Ingredient kristalsuiker  = new Ingredient (75, "gram", "kristalsuiker");
+    Ingredient kaneel  = new Ingredient (3, "theelepels", "kaneel");
+    Ingredient parneermeel  = new Ingredient (15, "gram", "parneermeel");
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!"
-        );
+       ApplePieRecipe applePie = new ApplePieRecipe();
+        System.out.println("ingredient = " + applePie.roomboter.getName());
+        System.out.println("amount = " + applePie.roomboter.getAmount());
+        System.out.println(" unit " + applePie.roomboter.getUnit());
+        System.out.println( applePie.roomboter.getAmount() +" " +applePie.roomboter.getUnit()+ " "+ applePie.roomboter.getName() );
     }
 }
